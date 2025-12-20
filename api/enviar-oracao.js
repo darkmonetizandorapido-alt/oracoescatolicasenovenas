da7b7dd71d5e4e20b33c61da2f45efa72852b0b3
darkmonetizandorapido-alt <darkmonetizandorapido@gmail.com>
2025-12-20 01:50:15 -0300

    Update enviar-oracao.js

--- api/enviar-oracao.js
+++ api/enviar-oracao.js
@@ -1,3 +1,11 @@
+export default function handler(req, res) {
+  return res.status(200).json({
+    user: process.env.EMAILJS_USER_ID || null,
+    service: process.env.EMAILJS_SERVICE_ID || null,
+    template: process.env.EMAILJS_TEMPLATE_ID || null
+  });
+}
+
 const fetch = require('node-fetch');
 
 module.exports = async (req, res) => {
